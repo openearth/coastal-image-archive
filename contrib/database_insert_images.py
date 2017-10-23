35c6c6eff20317073dd86e627859516a5da50897
Kees den Heijer <kees.denheijer@deltares.nl>
2017-10-23 10:13:41 +0200

    apache config for django adjusted and some improvements in database_insert_images.py

--- contrib/database_insert_images.py
+++ contrib/database_insert_images.py
@@ -13,19 +13,32 @@ class ImageList:
     site = None
     siteid = None
     mmin = -1440
+    cam_min = None
+    cam_max = None
     database_default_file = '~/.my.cnf'
 
-    def __init__(self, site, siteid=None):
+    def __init__(self, site, siteid=None, **kwargs):
         self.site = site
         if siteid:
             self.siteid = siteid
         else:
             self.siteid = site
+        for key, val in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, val)
         self.set_mmin()
         self.create_image_list()
         self.read_image_list()
      
         self.database_insert()
+
+    def _check_file_existance(self, filename):
+        if not os.path.exists(filename):
+            raise IOError('file not found: %s' % filename)
+
+    def set_database_default_file(self, database_default_file):
+        self._check_file_existance(database_default_file)
+        self.database_default_file = database_default_file
 
     def create_image_list(self):
         # REMOTE_USER variable is work-around to not hard-code the user
@@ -33,16 +46,24 @@ class ImageList:
         os.system('REMOTE_USER=$(ssh %s whoami) && ssh %s ~$REMOTE_USER/bin/image_list.sh %+i > %s' % (self.siteid, self.siteid, self.mmin, self.filename))
 
     def read_image_list(self):
+        self._check_file_existance(self.filename)
         with open(self.filename) as fobj:
             txt = fobj.read()
         self.image_list = txt.split()
 
     def set_mmin(self):
+        self._check_file_existance(self.database_default_file)
         db = MySQLdb.connect(read_default_file=self.database_default_file)
 
         cur = db.cursor()
 
-        select_sql = "SELECT epoch FROM Images WHERE site='%s' ORDER BY epoch DESC LIMIT 1" % self.site
+        where_clause = "site='%s'" % self.site
+        if self.cam_min is not None:
+            where_clause = '%s AND camera>=%i' % (where_clause, self.cam_min)
+        if self.cam_max is not None:
+            where_clause = '%s AND camera<=%i' % (where_clause, self.cam_max)
+
+        select_sql = "SELECT epoch FROM Images WHERE %s ORDER BY epoch DESC LIMIT 1" % where_clause
 
         cur.execute(select_sql)
         if cur.rowcount > 0:
@@ -53,13 +74,18 @@ class ImageList:
         db.close()
 
     def database_insert(self):
+        self._check_file_existance(self.database_default_file)
         db = MySQLdb.connect(read_default_file=self.database_default_file)
 
         cur = db.cursor()
         n = 0
         for image in self.image_list:
             I = ImageFormat(filename=image, site=self.site)
-            select_sql = "SELECT location FROM Images WHERE location='%s'" % I.get_long()
+            try:
+                select_sql = "SELECT location FROM Images WHERE location='%s'" % I.get_long()
+            except:
+                print 'ERROR: filename: %s' % image
+                continue
             cur.execute(select_sql)
             if cur.rowcount == 0:
                 insert_sql = "INSERT INTO Images (location,site,epoch,camera,type,dayminute) " + \
@@ -79,8 +105,17 @@ def main():
     group.add_argument('-s', '--site', help='name of site')
     group.add_argument('-c', '--config-file', help='yml file containing site information')
     parser.add_argument('-i', '--site-id', help='id of site (defaults to name of site)')
+    parser.add_argument('-m', '--mysql-config', help='mysql config file (defaults to ~/.my.cnf)')
 
     args = parser.parse_args()
+
+    if args.mysql_config:
+        if os.path.exists(args.mysql_config):
+            kwargs = {'database_default_file': args.mysql_config}
+        else:
+            raise IOError('file not found: %s' % args.mysql_config)
+    else:
+        kwargs = {}
 
     if args.config_file:
         if os.path.exists(args.config_file):
@@ -92,12 +127,17 @@ def main():
                     siteid = site['id']
                 else:
                     siteid = None
-                IL = ImageList(site=site['site'], siteid=siteid)
+                cam_min, cam_max = None, None
+                if 'cam_min' in site:
+                    cam_min = site['cam_min']
+                if 'cam_max' in site:
+                    cam_max = site['cam_max']
+                IL = ImageList(site=site['site'], siteid=siteid, cam_min=cam_min, cam_max=cam_max, **kwargs)
         else:
             raise IOError('file not found: %s' % args.config_file)
 
     if args.site:
-        IL = ImageList(site=args.site, siteid=args.site_id)
+        IL = ImageList(site=args.site, siteid=args.site_id, **kwargs)
 
 
 if __name__ == '__main__':
