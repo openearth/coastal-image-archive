9d708e66647639bb23e266718f9b1ca0c7407a97
Kees den Heijer <kees.denheijer@deltares.nl>
2018-03-07 15:26:56 +0100

    database_insert_images.py retrieve all metadata option added as opposed to the default incremental behaviour

--- contrib/database_insert_images.py
+++ contrib/database_insert_images.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 import argparse
 from image_format import ImageFormat
 import MySQLdb
@@ -8,34 +9,34 @@ import datetime
 
 
 class ImageList:
-    filename = '/tmp/image_list.txt'
     image_list = None
     site = None
     siteid = None
     mmin = -1440
     database_default_file = '~/.my.cnf'
 
-    def __init__(self, site, siteid=None):
+    def __init__(self, site, siteid=None, mmin=True):
         self.site = site
         if siteid:
             self.siteid = siteid
         else:
             self.siteid = site
-        self.set_mmin()
-        self.create_image_list()
-        self.read_image_list()
-     
+        if mmin:
+            self.set_mmin()
+        else:
+            self.mmin = 0
+        self.retrieve_image_list()
+
         self.database_insert()
 
-    def create_image_list(self):
+    def retrieve_image_list(self):
         # REMOTE_USER variable is work-around to not hard-code the user
         # just using ~ will try to use the local user
-        os.system('REMOTE_USER=$(ssh %s whoami) && ssh %s ~$REMOTE_USER/bin/image_list.sh %+i > %s' % (self.siteid, self.siteid, self.mmin, self.filename))
-
-    def read_image_list(self):
-        with open(self.filename) as fobj:
-            txt = fobj.read()
-        self.image_list = txt.split()
+        remote_user = subprocess.check_output(['ssh', self.siteid, 'whoami']).strip()
+        image_list = subprocess.check_output(['ssh', self.siteid,
+                                              '~%s/bin/image_list.sh' % remote_user, '%+i' % self.mmin]
+                                             ).strip().split('\n')
+        self.image_list = image_list
 
     def set_mmin(self):
         db = MySQLdb.connect(read_default_file=self.database_default_file)
@@ -59,17 +60,21 @@ class ImageList:
         n = 0
         for image in self.image_list:
             I = ImageFormat(filename=image, site=self.site)
+            if I.format is None:
+                continue
             select_sql = "SELECT location FROM Images WHERE location='%s'" % I.get_long()
             cur.execute(select_sql)
             if cur.rowcount == 0:
                 insert_sql = "INSERT INTO Images (location,site,epoch,camera,type,dayminute) " + \
-                             "VALUES ('%s', '%s', %i, %i, '%s', %i)" % (I.get_long(), self.site, I.epoch, I.camera, I.image_type, I.dayminute)
+                             "VALUES ('%s', '%s', %i, %i, '%s', %i)" % (
+                                 I.get_long(), self.site, I.epoch, I.camera, I.image_type, I.dayminute)
                 cur.execute(insert_sql)
                 db.commit()
                 n += 1
         db.close()
         print datetime.datetime.utcnow().strftime('%c UTC')
-        print "%i new images found on station %s (%s) over the last %i minutes" % (len(self.image_list), self.site, self.siteid, -self.mmin)
+        print "%i new images found on station %s (%s) over the last %i minutes" % (
+            len(self.image_list), self.site, self.siteid, -self.mmin)
         print "details of %i new images added to the database" % n
 
 
@@ -79,8 +84,11 @@ def main():
     group.add_argument('-s', '--site', help='name of site')
     group.add_argument('-c', '--config-file', help='yml file containing site information')
     parser.add_argument('-i', '--site-id', help='id of site (defaults to name of site)')
+    parser.add_argument('-a', '--all', action='store_true',
+                        help='retrieve information of all images from remote station (default is incremental since the latest image found in the database)')
 
     args = parser.parse_args()
+    mmin = not args.all
 
     if args.config_file:
         if os.path.exists(args.config_file):
@@ -92,12 +100,12 @@ def main():
                     siteid = site['id']
                 else:
                     siteid = None
-                IL = ImageList(site=site['site'], siteid=siteid)
+                IL = ImageList(site=site['site'], siteid=siteid, mmin=mmin)
         else:
             raise IOError('file not found: %s' % args.config_file)
 
     if args.site:
-        IL = ImageList(site=args.site, siteid=args.site_id)
+        IL = ImageList(site=args.site, siteid=args.site_id, mmin=mmin)
 
 
 if __name__ == '__main__':
